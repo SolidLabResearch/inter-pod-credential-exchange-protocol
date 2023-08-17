0306deb7f4f8f58ba870ca961d9919e18f6c83fa
JSteinbach <jsteinbach@mailbox.org>
2023-08-17 11:01:44 +0200

    Check server status

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const {checkCssIsAvailable} = require("./server")
+const { checkCssIsAvailable } = require("./server")
 const { setupSigner, signerCreateAndSignCredential, signerSendCredentialToHolder } = require("./signer")
 const { setupHolder, holderDeriveProof, holderSendProofToVerifier } = require("./holder")
 const { setupVerifier, verifierRequestProofFromHolder, verifierVerify } = require("./verifier")
 
 // Q: better start all pods at the same time or have them run separate?
 const demo = async () => {
-    // 0. make sure CSS runs and is available
-    // - reset everything?
-    checkCssIsAvailable()
+  // 0. make sure CSS runs and is available
+  // - reset everything? -> using in-memory config; always resets
+  await checkCssIsAvailable()
 
-    // 1. setup pods 
-    // - empty/reset them
-    // - register/create user and password
-    // - put (possible) default resources
-    // - ACP/WAC? (WAC is easier, ACP does a bit more)
-    // - have them run idle
-    // the setup scripts are separate, as we 
-    setupSigner()
-    setupHolder()
-    setupVerifier()
+  // 1. setup pods 
+  // - empty/reset them
+  // - register/create user and password
+  // - put (possible) default resources
+  // - ACP/WAC? (WAC is easier, ACP does a bit more)
+  // - have them run idle
+  // the setup scripts are separate, as we 
+  setupSigner()
+  setupHolder()
+  setupVerifier()
 
-    // TODO: wait for signer, holder and verifier to be setup, then continue
+  // TODO: wait for signer, holder and verifier to be setup, then continue
 
-    // 2a. Signer -> Holder
-    signerCreateAndSignCredential()
-    signerSendCredentialToHolder()
+  // 2a. Signer -> Holder
+  signerCreateAndSignCredential()
+  signerSendCredentialToHolder()
 
-    // TODO: only continue after the Holder holds the credential
-    //  -> simulate the interaction between Holder and Verifier?
+  // TODO: only continue after the Holder holds the credential
+  //  -> simulate the interaction between Holder and Verifier?
 
-    // 2b. Holder -> Verifier
-    verifierRequestProofFromHolder()
-    holderDeriveProof()
-    holderSendProofToVerifier()
-    verifierVerify()
+  // 2b. Holder -> Verifier
+  verifierRequestProofFromHolder()
+  holderDeriveProof()
+  holderSendProofToVerifier()
+  verifierVerify()
 }
 
 demo()
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+const serverUrl = 'http://localhost:3000/'
+const serverIdp = serverUrl + 'idp/register/'
 
-const checkCssIsAvailable = () => {}
+const checkCssIsAvailable = async () => {
+  fetch(serverUrl, {
+    method: "GET",
+  }).then(response => response)
+    .then(data => {
+      console.log("server is up")
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      console.log("aborting; server is down!")
+      process.exit()
+    });
+}
 
 
-module.exports = {checkCssIsAvailable}
+module.exports = { checkCssIsAvailable }
