4cd2b0589099e2f3cdfe9c2e8e754a756b1ccd55
JSteinbach <jsteinbach@mailbox.org>
2024-03-16 14:22:09 +0100

    fix headers

--- src/holder.js
+++ src/holder.js
@@ -1,78 +1,87 @@
-const { login } = require('./util/login.js')
-const { serverUrls } = require('./server.js')
-const { setupInbox, discoverNotifications, discoverInbox, getNotification } = require('./util/notifications.js')
-const { putResourceOnPod } = require('./util/util.js')
+const { login } = require("./util/login.js");
+const { serverUrls } = require("./server.js");
+const {
+  setupInbox,
+  discoverNotifications,
+  discoverInbox,
+  getNotification,
+} = require("./util/notifications.js");
+const { putResourceOnPod } = require("./util/util.js");
 
 // TODO: read these from the `seed-pods.json` file?
 const holder_credentials = {
   podName: "holder",
   email: "holder@solid.server",
   password: "holder",
-  webId: 'http://localhost:3000/holder/profile/card#me'
-}
-const podUrl = serverUrls.baseUrl + holder_credentials.podName + '/' // the '/' is importan!
-const webIdProfileUrl = podUrl + 'profile/card'
-const originalDocumentUrl = podUrl + 'private/original'
-const derivationUrl = podUrl + 'private/derivation'
+  webId: "http://localhost:3000/holder/profile/card#me",
+};
+const podUrl = serverUrls.baseUrl + holder_credentials.podName + "/"; // the '/' is importan!
+const webIdProfileUrl = podUrl + "profile/card";
+const originalDocumentUrl = podUrl + "private/original";
+const derivationUrl = podUrl + "private/derivation";
 
 const setupHolder = async () => {
   // 1. login and get/create the auth fetch
-  const { accessToken, dpopKey, authFetch } = await login(holder_credentials)
-  console.log('got credentials for %s', holder_credentials.podName)
+  const { accessToken, dpopKey, authFetch } = await login(holder_credentials);
+  console.log("got credentials for %s", holder_credentials.podName);
 
   // 2. create LDN inbox
-  const inboxUrl = await setupInbox(authFetch, podUrl, webIdProfileUrl)
-  console.log('holder inbox is at %s', inboxUrl)
+  const inboxUrl = await setupInbox(authFetch, podUrl, webIdProfileUrl);
+  console.log("holder inbox is at %s", inboxUrl);
 
-  return authFetch
-}
+  return authFetch;
+};
 
 const holderReceiveCredentials = async (authFetch) => {
-  console.log('Discovering notifications')
+  console.log("Discovering notifications");
   // 1. discover all the notifications in the inbox
-  const inboxUrl = await discoverInbox(holder_credentials.webId)
-  const notificationUrls = await discoverNotifications(inboxUrl)
+  const inboxUrl = await discoverInbox(holder_credentials.webId);
+  const notificationUrls = await discoverNotifications(inboxUrl);
 
   // 2. read the notifications one-by-one
   // - delete/mark read?
   // - find the first notification from the Signer?
   // - how do we know we are looking for something from the Signer? -> There is another implicit interaction between Signer and Holder
-  console.log('readings notifications')
+  console.log("readings notifications");
   // find the notification from the Signer
   const match = notificationUrls.find(async (url) => {
-    const notificationUrl = inboxUrl + url
-    const notification = await getNotification(notificationUrl)
-    const notificationText = await notification.text()
-    const notificationJson = await JSON.parse(notificationText)
+    const notificationUrl = inboxUrl + url;
+    const notification = await getNotification(notificationUrl);
+    const notificationText = await notification.text();
+    const notificationJson = await JSON.parse(notificationText);
 
-    if (notificationJson.sender = 'http://localhost:3000/signer/profile/card#me') {
-      console.log('Found a match! %s', notificationUrl)
+    if (
+      (notificationJson.from = "http://localhost:3000/signer/profile/card#me")
+    ) {
+      console.log("Found a match! %s", notificationUrl);
 
       // 3. save the signed document at a secure place
-      const result = await putResourceOnPod(authFetch, originalDocumentUrl, notificationJson.content)
-      console.log(result.status)
+      const result = await putResourceOnPod(
+        authFetch,
+        originalDocumentUrl,
+        notificationJson.content,
+      );
+      console.log(result.status);
 
       // 3-post. verify the result
-      const v = await authFetch(originalDocumentUrl)
-      console.log('read stored document %s', v.status)
-      console.log(v.headers)
-      console.log(await v.text())
+      const v = await authFetch(originalDocumentUrl);
+      console.log("read stored document %s", v.status);
+      console.log(v.headers);
+      console.log(await v.text());
 
-
-      return true
+      return true;
     }
-    return false
+    return false;
+  });
+  console.log("FOUND! %s", match);
+};
 
-  })
-  console.log('FOUND! %s', match)
+const holderDeriveProof = () => {};
+const holderSendProofToVerifier = () => {};
 
-}
-
-
-
-const holderDeriveProof = () => { }
-const holderSendProofToVerifier = () => { }
-
-
-
-module.exports = { setupHolder, holderDeriveProof, holderSendProofToVerifier, holderReceiveCredentials }
+module.exports = {
+  setupHolder,
+  holderDeriveProof,
+  holderSendProofToVerifier,
+  holderReceiveCredentials,
+};
--- src/signer.js
+++ src/signer.js
@@ -1,9 +1,13 @@
-const { login } = require('./util/login.js')
-const { setupInbox, discoverInbox, sendMessage } = require('./util/notifications.js')
-const { signDocument } = require('./util/signatures.js')
-const { putResourceOnPod } = require('./util/util.js')
-const { serverUrls } = require('./server.js')
-const inputDocument = require("../data/inputDocument.json")
+const { login } = require("./util/login.js");
+const {
+  setupInbox,
+  discoverInbox,
+  sendMessage,
+} = require("./util/notifications.js");
+const { signDocument } = require("./util/signatures.js");
+const { putResourceOnPod } = require("./util/util.js");
+const { serverUrls } = require("./server.js");
+const inputDocument = require("../data/inputDocument.json");
 
 // the signer signs the credential document
 // he/she/it knows the identity of the holder and is authorized/trusted to issue a document about the holder
@@ -18,39 +22,48 @@ const signer_credentials = {
   podName: "signer",
   email: "signer@solid.server",
   password: "signer",
-  webId: 'http://localhost:3000/signer/profile/card#me'
-}
-const podUrl = serverUrls.baseUrl + signer_credentials.podName + '/' // the '/' is importan!
-const webIdProfileUrl = podUrl + 'profile/card'
-const initialDocUrl = podUrl + 'private/initialDoc'
+  webId: "http://localhost:3000/signer/profile/card#me",
+};
+const podUrl = serverUrls.baseUrl + signer_credentials.podName + "/"; // the '/' is importan!
+const webIdProfileUrl = podUrl + "profile/card";
+const initialDocUrl = podUrl + "private/initialDoc";
 
 const setupSigner = async () => {
   // 1. get/create the auth fetch
-  const { accessToken, dpopKey, authFetch } = await login(signer_credentials)
-  console.log('got credentials for %s', signer_credentials.podName)
+  const { accessToken, dpopKey, authFetch } = await login(signer_credentials);
+  console.log("got credentials for %s", signer_credentials.podName);
 
   // 2-pre: discover pod description resources
   // TODO HEAD to get the .meta, then discover the correct location
   // discoverDescriptionResource(authFetch, podUrl)
   // 2. create LDN inbox
-  const inboxUrl = await setupInbox(authFetch, podUrl, webIdProfileUrl)
+  const inboxUrl = await setupInbox(authFetch, podUrl, webIdProfileUrl);
 
   // 3. put initial doc on the pod
-  const result = await putResourceOnPod(authFetch, initialDocUrl, inputDocument)
-  console.log('PUT intial document on %s\'s pod. Status: %s - %s', signer_credentials.podName, result.status, result.statusText)
+  const result = await putResourceOnPod(
+    authFetch,
+    initialDocUrl,
+    inputDocument,
+  );
+  console.log(
+    "PUT intial document on %s's pod. Status: %s - %s",
+    signer_credentials.podName,
+    result.status,
+    result.statusText,
+  );
 
-  return authFetch
-}
+  return authFetch;
+};
 
 const signerCreateAndSignCredential = async () => {
-  const result = await signDocument(inputDocument)
-  console.log('Verificaton result: %s', result.verificationResult.verified)
+  const result = await signDocument(inputDocument);
+  console.log("Verificaton result: %s", result.verificationResult.verified);
   if (!result.verificationResult.verified) {
-    console.log("aborting; could not sign document!")
-    process.exit()
+    console.log("aborting; could not sign document!");
+    process.exit();
   }
-  return result
-}
+  return result;
+};
 
 // pre. signer knows Alice's WebID (=holder)
 // 1. signer fetches Alice's WebID to discover her inbox
@@ -58,13 +71,23 @@ const signerCreateAndSignCredential = async () => {
 const signerSendCredentialToHolder = async (authFetch, signedDocument) => {
   // 1. discover inbox
   // TODO: can we can really assume Holders/Alice's WebId?
-  const holderWebId = 'http://localhost:3000/holder/profile/card#me'
-  const inboxUrl = await discoverInbox(holderWebId)
+  const holderWebId = "http://localhost:3000/holder/profile/card#me";
+  const inboxUrl = await discoverInbox(holderWebId);
 
   // 1-post: send document
   // TODO: JSON-LD?
-  await sendMessage(inboxUrl, { content: signedDocument, from: signer_credentials.webId, dateCreated: new Date().toISOString() })
-}
+  // TODO: properly generate UUID
+  await sendMessage(inboxUrl, {
+    id: Date.now().toString(36) + Math.random().toString(36).substring(2),
+    from: signer_credentials.webId,
+    to: holderWebId,
+    created_time: new Date().toISOString(),
+    body: signedDocument,
+  });
+};
 
-
-module.exports = { setupSigner, signerCreateAndSignCredential, signerSendCredentialToHolder }
+module.exports = {
+  setupSigner,
+  signerCreateAndSignCredential,
+  signerSendCredentialToHolder,
+};
